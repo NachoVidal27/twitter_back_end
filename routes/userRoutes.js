a8da52629a27fdd1f9d1d049488a825b9985b628
simonabadcat <114427654+simonabadcat@users.noreply.github.com>
2023-03-19 15:44:04 -0300

    se agrega middleware a las rutas de usuario

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
-// const isAuthenticated = require("../middlewares/ensureAuthenticated");
+const { expressjwt: checkJwt } = require("express-jwt");
 
-router.post("/", userController.store);
 router.post("/tokens", userController.getToken);
+router.use(checkJwt({ secret: process.env.SESSION_SECRET, algorithms: ["HS256"] }));
+router.post("/", userController.store);
 router.get("/:id", userController.userTweets);
 router.post("/follow", userController.userFollow);
 
